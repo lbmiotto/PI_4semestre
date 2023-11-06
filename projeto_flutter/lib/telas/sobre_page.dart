71c9eeb94b3b265a299902411b0a9fba409171d4
LUCAS BARBAROTO MIOTTO <22.01388-0@maua.br>
2023-11-06 08:03:58 -0300

    Ajuste de Overboard 2

--- projeto_flutter/lib/telas/sobre_page.dart
+++ projeto_flutter/lib/telas/sobre_page.dart
@@ -143,41 +143,40 @@ class SobrePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-                'assets/img/sobre.png'),
-            SizedBox(height: 20),
-            Container(
-              constraints: BoxConstraints(
-                  maxWidth: 500),
-              child: Text(
-                'Um pouco mais sobre a nossa Aplicação!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              margin: EdgeInsets.all(20),
-              constraints: BoxConstraints(
-                  maxWidth: 900),
-              child: Center(
+      body: ListView(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/img/sobre.png'),
+              SizedBox(height: 20),
+              Container(
+                constraints: BoxConstraints(maxWidth: 500),
                 child: Text(
-                  'A aplicação oferece um ambiente abrangente e versátil que abrange quatro funções essenciais: gerenciamento de usuários, gerenciamento de organização, gerenciamento de projetos e controle de dados. Com o poder de gerenciar a equipe, organizar a estrutura da organização, supervisionar projetos e manter a segurança dos dados, a aplicação proporciona uma solução completa para a administração eficaz de informações e recursos. Isso resulta em maior eficiência, colaboração e segurança, tornando-a uma ferramenta fundamental para empresas e organizações que buscam um controle preciso e abrangente de suas operações.',
-                  style: TextStyle(fontSize: 16),
+                  'Um pouco mais sobre a nossa Aplicação!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-          ],
-        ),
+              SizedBox(height: 20),
+              Container(
+                margin: EdgeInsets.all(20),
+                constraints: BoxConstraints(maxWidth: 900),
+                child: Center(
+                  child: Text(
+                    'A aplicação oferece um ambiente abrangente e versátil que abrange quatro funções essenciais: gerenciamento de usuários, gerenciamento de organização, gerenciamento de projetos e controle de dados. Com o poder de gerenciar a equipe, organizar a estrutura da organização, supervisionar projetos e manter a segurança dos dados, a aplicação proporciona uma solução completa para a administração eficaz de informações e recursos. Isso resulta em maior eficiência, colaboração e segurança, tornando-a uma ferramenta fundamental para empresas e organizações que buscam um controle preciso e abrangente de suas operações.',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ],
       ),
     );
   }
